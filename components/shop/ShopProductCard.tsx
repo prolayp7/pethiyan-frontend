93cc975f4470819479f551cad3b6ec8ad71b033b
prolaypanda <prolaypanda7@gmail.com>
2026-04-19 21:24:01 +0530

    Deploy sync 2026-04-19 21:24:01

--- components/shop/ShopProductCard.tsx
+++ components/shop/ShopProductCard.tsx
@@ -416,7 +416,6 @@ export default function ShopProductCard({ product }: { product: RealApiProduct }
               return (
                 <AttributePillsWithVariants
                   colors={colors}
-                  sizes={sizes.map((s) => ({ value: s.size, variantId: s.variantId }))}
                   weights={weights.map((w) => ({ value: w.weight, variantId: w.variantId }))}
                   hoveredVariantId={hoveredVariantId}
                   onHoverVariant={(id) => {
