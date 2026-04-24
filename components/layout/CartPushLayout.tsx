685bbb0417b8c2779db172a8e8db8a98aea41a7b
prolaypanda <prolaypanda7@gmail.com>
2026-04-24 11:02:28 +0530

    Deploy sync 2026-04-24 11:02:28

--- components/layout/CartPushLayout.tsx
+++ components/layout/CartPushLayout.tsx
@@ -1,18 +1,5 @@
 "use client";
 
-import { useCart } from "@/context/CartContext";
-
 export default function CartPushLayout({ children }: { children: React.ReactNode }) {
-  const { isOpen } = useCart();
-
-  return (
-    <div
-      style={{
-        marginRight: isOpen ? "24rem" : "0",
-        transition: "margin-right 500ms ease-in-out",
-      }}
-    >
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
