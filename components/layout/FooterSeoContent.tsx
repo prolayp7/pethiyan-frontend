6617cce376cb18c550475828d044ba55e34d5c46
prolaypanda <prolaypanda7@gmail.com>
2026-05-10 22:53:54 +0530

    Deploy sync 2026-05-10 22:53:54

--- components/layout/FooterSeoContent.tsx
+++ components/layout/FooterSeoContent.tsx
@@ -13,7 +13,7 @@ export default function FooterSeoContent({ introHtml }: FooterSeoContentProps) {
     <div className="bg-white border-y border-gray-200">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
         <div
-          className="text-sm leading-relaxed text-gray-500 [&_a]:text-blue-600 [&_a]:underline-offset-2 hover:[&_a]:underline"
+          className="footer-seo-prose"
           dangerouslySetInnerHTML={{ __html: resolvedIntroHtml }}
         />
       </div>
